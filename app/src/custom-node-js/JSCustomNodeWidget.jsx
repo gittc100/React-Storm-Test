5ca787d494ad1b8f5b8a7828b9cf78a51c2b01c3
austie702 <austie702@gmail.com>
2019-07-24 18:59:17 -0500

    make text editable for text screen area

--- app/src/custom-node-js/JSCustomNodeWidget.jsx
+++ app/src/custom-node-js/JSCustomNodeWidget.jsx
@@ -10,38 +10,59 @@ export class JSCustomNodeWidget extends React.Component {
 		this.ESCAPE_KEY = 27;
 		this.ENTER_KEY = 13;
 		this.state = {
-		  editText: "",
+			description: "",
+			title: "",
 			editing: false,
+			editingDesc: false,
 			};
 	  }
 
 	  componentDidMount(){
 		  this.setState({
 			  ...this.state,
-			  editText: this.props.node.name,
+				title: this.props.node.name,
+				description: this.props.node.description,
 		  });
 	  }
 	
-	  handleEdit (e) {
-		return (e) => this.setState({
-		  editing: !this.state.editing
-		});
+	  handleEdit (name) {
+			if (name === "description") {
+				this.setState({
+					editingDesc: !this.state.editingDesc
+				});
+			} else if (name === "title") {
+				this.setState({
+					editing: !this.state.editing
+				});
+			}	
 	  }  
 	  
 	  handleChange (e) {
-		this.setState({editText: e.target.value});
+			this.setState({
+				// editText: e.target.value,
+				[e.target.name]: e.target.value
+			});
 	  }
 	  
-	  handleSubmit (e) {
-		var val = this.state.editText;
+	  handleSubmit (event) {
+		var val = this.state[event.target.name];
 		if (val) {
-			this.setState({
-			  editText: val,
-				// editing: 'hidden'
-				editing: !this.state.editing
-			});
+			if (event.target.name === "description") {
+				this.setState({
+					...this.state,
+					[event.target.name]: val,
+					editingDesc: !this.state.editingDesc
+				});
+			} else if (event.target.name === "title") {
+				this.setState({
+					...this.state,
+					[event.target.name]: val,
+					editing: !this.state.editing
+				});
+			}	
+			
 		} 
-		this.props.node.name = this.state.editText;
+		this.props.node.name = this.state[event.target.name];
 		//  this.props.diagramEngine.repaintCanvas()
 		 console.log("this", this.props.node);
 		}
@@ -60,11 +81,13 @@ export class JSCustomNodeWidget extends React.Component {
 						<PortWidget node={this.props.node} name="in" />
 					</div>
 					<h1 
-						className={this.state.editing ? 'hidden' : ''} onDoubleClick={this.handleEdit()}>{this.state.editText}</h1>
-					<input 
+						className={this.state.editing ? 'hidden' : ''} onDoubleClick={()=>this.handleEdit("title")}>{this.state.title}
+					</h1>
+					<input
+					name="title" 
 					placeholder="Enter something..."
 					className={this.state.editing ? '' : 'hidden'} 
-					value={this.state.editText} 
+					value={this.state.title} 
 					onChange={this.handleChange.bind(this)} 
 					// onBlur={this.handleSubmit.bind(this)}
 					onKeyDown={this.handleKeyDown.bind(this)}
@@ -73,7 +96,18 @@ export class JSCustomNodeWidget extends React.Component {
         </div>
 				
 				<div className="custom-node-screen">
-					{this.props.node.description}
+					<p 
+						className={this.state.editingDesc ? 'hidden' : ''} onDoubleClick={()=>this.handleEdit("description")}>{this.state.description}
+					</p>
+					<textarea
+					name="description" 
+					placeholder="Enter something..."
+					className={this.state.editingDesc ? '' : 'hidden'} 
+					value={this.state.description} 
+					onChange={this.handleChange.bind(this)} 
+					// onBlur={this.handleSubmit.bind(this)}
+					onKeyDown={this.handleKeyDown.bind(this)}
+					/>
 				</div>
         
 				<div className="custom-node-submenus">
