c804b887f6148c0e90889540e2489a662b073010
austie702 <austie702@gmail.com>
2019-08-01 17:21:36 -0500

    all but delete and links are at 90%+

--- app/src/custom-node-js/JSCustomNodeModel.js
+++ app/src/custom-node-js/JSCustomNodeModel.js
@@ -1,129 +1,88 @@
-// import { DefaultPortModel, NodeModel } from '@projectstorm/react-diagrams';
+import {
+  DefaultPortModel,
+  NodeModel,
+  Toolkit
+} from "@projectstorm/react-diagrams";
+import lodash from "lodash";
 
-// /**
-//  * Example of a custom model using pure javascript
-//  */
-// export class JSCustomNodeModel extends NodeModel {
-// 	constructor(options = {}) {
-// 		super({
-// 			...options,
-// 			type: 'js-custom-node'
-// 		});
-// 		this.color = options.color || { options: 'red' };
-
-// 		// setup an in and out port
-// 		this.addPort(
-// 			new DefaultPortModel({
-// 				in: true,
-// 				name: 'in'
-// 			})
-// 		);
-// 		this.addPort(
-// 			new DefaultPortModel({
-// 				in: false,
-// 				name: 'out'
-// 			})
-// 		);
-// 	}
-
-// 	serialize() {
-// 		return {
-// 			...super.serialize(),
-// 			color: this.options.color
-// 		};
-// 	}
-
-// 	deSerialize(ob, engine) {
-// 		super.deSerialize(ob, engine);
-// 		this.color = ob.color;
-// 	}
-// }
-
-
-
-
-
-
-import {DefaultPortModel, NodeModel} from "@projectstorm/react-diagrams";
-// import * as _ from "lodash";
-/**
- * Example of a custom model using pure javascript
- */
 export class JSCustomNodeModel extends NodeModel {
+  constructor(options = {}) {
+    super({
+      ...options,
+      type: "js-custom-node"
+    });
 
-	constructor(options = {}) {
-		super({
-			...options,
-			type: 'js-custom-node'
-		});
-		// this.color = options.color || {options: 'red'};
-		// we made this
-		this.color = options.color || { options: 'red' };
-		// this.name = options.name;
-		// this.description = options.description;
-		// ------------
+    this.color = options.color || { options: "red" };
+    this.name = options.name;
+    this.description = options.description;
 
-		// setup an in and out port
-		this.addPort(
-			new DefaultPortModel({
-				in: true,
-				name: 'in'
-			})
-		);
-		this.addPort(
-			new DefaultPortModel({
-				in: false,
-				name: 'out'
-			})
-		);
-	}
+    // setup an in / out port
+    this.addPort(
+      new DefaultPortModel({
+        in: true,
+        name: "in"
+      })
+    );
+    this.addPort(
+      new DefaultPortModel({
+        in: false,
+        name: "out",
+        label: "Enter menu title..."
+      })
+    );
+  }
 
-	
-	// serialize() {
-	// 	return _.merge(super.serialize(), {
-	// 		name: this.name,
-	// 		description: this.description
-	// 	});
-	// }
-	
-	// deSerialize(object, engine) {
-	// 	super.deSerialize(object, engine);
-	// 	this.name = object.name;
-	// 	this.description = object.description;
-	// }
+  serialize() {
+    return {
+      ...super.serialize(),
+      color: this.options.color,
+      name: this.name,
+      description: this.description
+    };
+  }
 
-	serialize() {
-		return {
-			...super.serialize(),
-			color: this.options.color
-		};
-	}
+  deSerialize(ob, engine) {
+    super.deSerialize(ob, engine);
+    this.color = ob.color;
+    this.name = ob.name;
+    this.description = ob.description;
+  }
 
-	deSerialize(ob, engine) {
-		super.deSerialize(ob, engine);
-		this.color = ob.color;
-	}
-	
-	// nameNode(name){
-	// 	this.name = name;
-	// }
+  nameNode(name) {
+    this.name = name;
+  }
 
-	// provideDescription(description){
-	// 	this.description = description;
-	// }
+  provideDescription(description) {
+    this.description = description;
+  }
 
-	// addOutPort(label) {
-	// 	return this.addPort(new DefaultPortModel(false, Toolkit.UID(), label));
-	// }
-	
-	// removePortAndLinks(port) {
-	// 	//clear the parent node reference
-	// 	if (this.ports[port.name]) {
-	// 		_.forEach(port.getLinks(), link => {
-	// 			link.remove();
-	// 		});
-	// 		this.ports[port.name].setParent(null);
-	// 		delete this.ports[port.name];
-	// 	}
-	// }
+  addOutPort(label) {
+    console.log("addOutPort clicked");
+    return this.addPort(
+      new DefaultPortModel({
+        in: false,
+        name: Toolkit.UID(),
+        label: label
+      })
+    );
+  }
+
+  // removePort(port: PortModel) {
+  //   //clear the parent node reference
+  //   if (this.ports[port.getName()]) {
+  //     this.ports[port.getName()].setParent(null);
+  //     delete this.ports[port.getName()];
+  //   }
+  // }
+
+  // removePortAndLinks(port) {
+  //   //clear the parent node reference
+  //   if (this.ports[port.name]) {
+  //     _.forEach(port.getLinks(), link => {
+  //       link.remove();
+  //     });
+  //     this.ports[port.name].setParent(null);
+  //     delete this.ports[port.name];
+  //   }
+  // }
 }
--- app/src/main.js
+++ app/src/main.js
@@ -4,7 +4,6 @@ import "./main.css";
 import createEngine, {
   DefaultLinkModel,
   DiagramModel,
-  DefaultNodeModel,
   DefaultNodeFactory
 } from "@projectstorm/react-diagrams";
 import { JSCustomNodeFactory } from "./custom-node-js/JSCustomNodeFactory";
@@ -25,14 +24,9 @@ import { BodyWidget } from "./BodyWidget";
   // now create two nodes of each type, and connect them
 
   const node1 = new JSCustomNodeModel();
-  // node1.nameNode("Node 1");
-  // node1.provideDescription("Description node 2");
-  node1.setPosition(150, 300);
-
-  const node2 = new JSCustomNodeModel();
-  // node2.nameNode("Node 2");
-  // node2.provideDescription("Description node 2");
-  node2.setPosition(900, 150);
+  node1.nameNode("Node 1");
+  node1.provideDescription("Description node 2");
+  node1.setPosition(400, 50);
 
   // const node1 = new DefaultNodeModel();
   // // node1.nameNode("Node 1");
@@ -46,13 +40,12 @@ import { BodyWidget } from "./BodyWidget";
 
 
   // 3-C) link the 2 nodes together
-  const link1 = new DefaultLinkModel();
-  link1.setSourcePort(node1.getPort("out"));
-  link1.setTargetPort(node2.getPort("in"));
+  // const link1 = new DefaultLinkModel();
+  // link1.setSourcePort(node1.getPort("out"));
 
 
   // 4) add the models to the root graph
-  model.addAll(node1, node2, link1);
+  model.addAll(node1);
 
 	
   // install the model into the engine
